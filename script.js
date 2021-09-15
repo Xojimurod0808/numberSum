92b8eb32e63756fcb987edaca5ce3f38259e27b3
Xojimurod0808 <xacker9988@gmail.com>
2021-09-15 23:27:37 +0500

    first cammit

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/*--------------------------------Mashq 1------------------------------------- */
+
+
 let number = [1, 3, 5, 9];
 
 function numberSum(arr){
@@ -5,7 +8,25 @@ function numberSum(arr){
     for (let i = 0; i < arr.length; i++){
         sum = sum + arr[i]
     }
-    title.textContent = "sum - all" + "  " + sum
+    console.log("sum - all" + "  " + sum)
 }
 
 numberSum(number)
+
+/*--------------------------------Mashq 2------------------------------------- */
+let numbers = [3.02, -3.65, 5, -9];
+
+function numberRound(num){
+    let result = [];
+
+    for(let number of num){
+        if(number < 0){
+            result.push(Math.round(number)* -1)
+        }
+        else{
+            result.push(Math.round(number));
+        }
+    }
+    return result;
+}
+console.log(numberRound(numbers))
